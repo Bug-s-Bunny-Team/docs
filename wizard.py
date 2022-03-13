f1373fc2feade5a03e99ea5ee813ad18a9eed280
Marco Volpato <marco.volpato00@gmail.com>
2022-03-13 15:15:43 +0100

    add templated filename

--- wizard.py
+++ wizard.py
@@ -11,6 +11,9 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
+###############################################################
+
+
 TEMPLATES_FILE = Path('templates.yml')
 SRC_PATH = Path('src')
 
@@ -20,18 +23,36 @@ jinja_env.block_end_string = '%]'
 jinja_env.variable_start_string = '[['
 jinja_env.variable_end_string = ']]'
 
+###############################################################
+
+
+def dateformat(value: str) -> str:
+    return value.replace('/', '').replace('-', '')
+
+
+jinja_env.filters['dateformat'] = dateformat
+
+
+def slugify(value: str) -> str:
+    return value.replace(' ', '_')
+
+
+jinja_env.filters['slugify'] = slugify
+
+###############################################################
+
 
 class DocTemplate:
     def __init__(
-        self, 
-        title: str, 
-        prefix: str, 
+        self,
+        title: str,
         template: Path,
+        filename_template: str,
         extra_vars: Optional[dict] = None
     ):
+        self.filename_template = filename_template
         self.variables = {
             'title': title,
-            'prefix': prefix,
             'date': datetime.date.today().isoformat().replace('-', '/'),
             'author': 'Nomone Cognomone'
         }
@@ -54,16 +75,18 @@ class DocTemplate:
                 _variables[name] = res
         return _variables
 
-    def render_template(self, variables: dict):
+    def render_template(self, variables: dict) -> str:
         with open(self.template, 'r') as f:
             template = jinja_env.from_string(f.read())
         return template.render(variables)
 
+    def render_filename(self, variables: dict) -> str:
+        template = jinja_env.from_string(self.filename_template)
+        return template.render(variables)
+
     def create_new(self, variables: dict, filename: Optional[str] = None) -> Optional[Path]:
         if not filename:
-            prefix = variables['prefix']
-            date = variables['date'].replace('/', '')
-            filename = f'{prefix}_{date}.tex'
+            filename = self.render_filename(variables)
         path = SRC_PATH.joinpath(filename)
         if path.is_file():
             return None
@@ -71,6 +94,8 @@ class DocTemplate:
         with open(path, 'w') as f:
             f.write(rendered)
         return path
+
+###############################################################
 
 
 def parse_templates(templates_file: Path) -> dict:
@@ -97,26 +122,32 @@ def parse_templates(templates_file: Path) -> dict:
 def main():
     templates = parse_templates(TEMPLATES_FILE)
 
-    template_name = Bullet('Select a template: ', choices=[t['title'] for t in templates]).launch()
+    template_name = Bullet(
+        'Select a template: ',
+        choices=[t['title'] for t in templates]
+    ).launch()
     template = next(filter(lambda t: t['title'] == template_name, templates))
-    
+
     template = DocTemplate(
-        template['title'], 
-        template['prefix'], 
-        Path(template['template']),
-        template.get('extra_vars')
+        title=template['title'],
+        template=Path(template['template']),
+        filename_template=template['filename_template'],
+        extra_vars=template.get('extra_vars')
     )
 
     variables = template.ask_variables()
     print(variables)
 
-    proceed = YesNo('Are you sure to create a new document from template? ').launch()
+    proceed = YesNo(
+        'Are you sure to create a new document from template? ').launch()
     if proceed:
         path = template.create_new(variables)
         if not path:
             print('Cannot create because it already exists.')
         else:
             print(f'Successfuly created new document at \'{path}\'')
+
+###############################################################
 
 
 if __name__ == '__main__':
